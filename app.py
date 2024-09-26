8b9820beaf4e4596b65a5a8efae64a578867810c
Yuvraj <yuvrajsankilwarofficial@gmail.com>
2024-09-26 18:44:28 +0530

    Third Commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from flask import Flask, request, app, jsonify, render_template, url_for
+from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
@@ -26,6 +26,26 @@ def predict_api():
     output = regmodel.predict(newdata)
     print(output)
     return jsonify(output[0])
+
+
+@app.route('/predict', methods=['POST'])
+def final_price():
+    # Collecting the data from the form
+    data = [float(request.form['MedInc']),
+            float(request.form['HouseAge']),
+            float(request.form['AveRooms']),
+            float(request.form['AveBedrms']),
+            float(request.form['Population']),
+            float(request.form['AveOccup']),
+            float(request.form['Latitude']),
+            float(request.form['Longitude'])]
+
+    # Transforming and predicting
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
+    output = regmodel.predict(final_input)[0]
+    
+    # Display the result
+    return render_template("home.html", prediction_text=f"The House price is ${output:.2f}")
 
 
 if __name__ == "__main__":
